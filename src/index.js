00e195ad7f31d754ac3794b693fd89830190fdc4
Anand Undavia <abundavia@gmail.com>
2019-06-09 20:25:42 +0530

    restart

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const { port, env } = require('./constants');
 const app = require('./config/express.config');
 const logger = require('./api/utils/logger')(__filename);
 
-// listen to requests
+// listen to requests.
 app.listen(port, (err) => {
 	if (err) {
 		return logger.error('server failed to start', err);
